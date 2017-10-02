62a0494b058eec24c0aef4763f10040a8b2ae8f3
Logan Cooper <ldtcoop@gmail.com>
2017-10-02 09:59:58 -0700

    Finish webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -8,6 +14,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-     ]
- }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
