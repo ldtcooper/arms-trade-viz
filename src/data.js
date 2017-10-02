5fa3b04a1fe60e519883099c33d2beba8a2b93d5
Logan Cooper <ldtcoop@gmail.com>
2017-10-02 16:33:05 -0700

    Convert data to JS objects

---
+++ src/data.js
@@ -0,0 +1,413 @@
+export const exportString = {
+   "Afghanistan": {
+      "Total": 1954
+   },
+   "Algeria": {
+      "Total": 171
+   },
+   "Angola": {
+      "Total": 1
+   },
+   "Argentina": {
+      "Total": 142
+   },
+   "Australia": {
+      "Total": 7603
+   },
+   "Austria": {
+      "Total": 65
+   },
+   "Azerbaijan": {
+      "Total": 4
+   },
+   "Bahamas": {
+      "Total": 4
+   },
+   "Bahrain": {
+      "Total": 295
+   },
+   "Bangladesh": {
+      "Total": 110
+   },
+   "Belgium": {
+      "Total": 148
+   },
+   "Bolivia": {
+      "Total": 8
+   },
+   "Botswana": {
+      "Total": 19
+   },
+   "Brazil": {
+      "Total": 683
+   },
+   "Brunei": {
+      "Total": 88
+   },
+   "Bulgaria": {
+      "Total": 14
+   },
+   "Burundi": {
+      "Total": 3
+   },
+   "Cameroon": {
+      "Total": 13
+   },
+   "Canada": {
+      "Total": 2800
+   },
+   "Central African Republic": {
+      "Total": 9
+   },
+   "Chad": {
+      "Total": 7
+   },
+   "Chile": {
+      "Total": 801
+   },
+   "Colombia": {
+      "Total": 1259
+   },
+   "Congo": {
+      "Total": 1
+   },
+   "Costa Rica": {
+      "Total": 2
+   },
+   "Croatia": {
+      "Total": 98
+   },
+   "Czech Republic": {
+      "Total": 153
+   },
+   "Denmark": {
+      "Total": 568
+   },
+   "Djibouti": {
+      "Total": 5
+   },
+   "Dominican Republic": {
+      "Total": 50
+   },
+   "DR Congo": {
+      "Total": 28
+   },
+   "Ecuador": {
+      "Total": 14
+   },
+   "Egypt": {
+      "Total": 6005
+   },
+   "El Salvador": {
+      "Total": 31
+   },
+   "Equatorial Guinea": {
+      "Total": 18
+   },
+   "Estonia": {
+      "Total": 15
+   },
+   "Ethiopia": {
+      "Total": 10
+   },
+   "Finland": {
+      "Total": 353
+   },
+   "France": {
+      "Total": 420
+   },
+   "Gabon": {
+      "Total": 1
+   },
+   "Georgia": {
+      "Total": 29
+   },
+   "Germany (FRG)": {
+      "Total": 1355
+   },
+   "Ghana": {
+      "Total": 18
+   },
+   "Greece": {
+      "Total": 4424
+   },
+   "Honduras": {
+      "Total": 4
+   },
+   "Hungary": {
+      "Total": 83
+   },
+   "India": {
+      "Total": 3029
+   },
+   "Indonesia": {
+      "Total": 512
+   },
+   "Iraq": {
+      "Total": 4151
+   },
+   "Ireland": {
+      "Total": 6
+   },
+   "Israel": {
+      "Total": 6408
+   },
+   "Italy": {
+      "Total": 2687
+   },
+   "Jamaica": {
+      "Total": 6
+   },
+   "Japan": {
+      "Total": 6069
+   },
+   "Jordan": {
+      "Total": 678
+   },
+   "Kazakhstan": {
+      "Total": 40
+   },
+   "Kenya": {
+      "Total": 22
+   },
+   "Kuwait": {
+      "Total": 1615
+   },
+   "Latvia": {
+      "Total": 67
+   },
+   "Lebanon": {
+      "Total": 143
+   },
+   "Libya": {
+      "Total": 8
+   },
+   "Lithuania": {
+      "Total": 31
+   },
+   "Luxembourg": {
+      "Total": 1
+   },
+   "Macedonia (FYROM)": {
+      "Total": 0
+   },
+   "Malaysia": {
+      "Total": 124
+   },
+   "Malta": {
+      "Total": 15
+   },
+   "Mauritania": {
+      "Total": 2
+   },
+   "Mauritius": {
+      "Total": 1
+   },
+   "Mexico": {
+      "Total": 1182
+   },
+   "Morocco": {
+      "Total": 1268
+   },
+   "Namibia": {
+      "Total": 5
+   },
+   "Netherlands": {
+      "Total": 1322
+   },
+   "New Zealand": {
+      "Total": 280
+   },
+   "Niger": {
+      "Total": 11
+   },
+   "Nigeria": {
+      "Total": 150
+   },
+   "Norway": {
+      "Total": 940
+   },
+   "Oman": {
+      "Total": 1192
+   },
+   "Pakistan": {
+      "Total": 3307
+   },
+   "Panama": {
+      "Total": 20
+   },
+   "Paraguay": {
+      "Total": 2
+   },
+   "Peru": {
+      "Total": 162
+   },
+   "Philippines": {
+      "Total": 348
+   },
+   "Poland": {
+      "Total": 2033
+   },
+   "Portugal": {
+      "Total": 622
+   },
+   "Qatar": {
+      "Total": 1690
+   },
+   "Romania": {
+      "Total": 200
+   },
+   "Saudi Arabia": {
+      "Total": 8367
+   },
+   "Senegal": {
+      "Total": 4
+   },
+   "Serbia": {
+      "Total": 0
+   },
+   "Seychelles": {
+      "Total": 1
+   },
+   "Singapore": {
+      "Total": 5238
+   },
+   "Slovenia": {
+      "Total": 1
+   },
+   "Somalia": {
+      "Total": 1
+   },
+   "South Africa": {
+      "Total": 103
+   },
+   "South Korea": {
+      "Total": 11584
+   },
+   "South Sudan": {
+      "Total": 2
+   },
+   "Spain": {
+      "Total": 1135
+   },
+   "Sri Lanka": {
+      "Total": 37
+   },
+   "Sweden": {
+      "Total": 627
+   },
+   "Switzerland": {
+      "Total": 84
+   },
+   "Taiwan (ROC)": {
+      "Total": 5226
+   },
+   "Thailand": {
+      "Total": 579
+   },
+   "Trinidad and Tobago": {
+      "Total": 32
+   },
+   "Tunisia": {
+      "Total": 207
+   },
+   "Turkey": {
+      "Total": 4667
+   },
+   "UAE": {
+      "Total": 9378
+   },
+   "Uganda": {
+      "Total": 4
+   },
+   "Ukraine": {
+      "Total": 4
+   },
+   "United Kingdom": {
+      "Total": 5354
+   },
+   "Uzbekistan": {
+      "Total": 35
+   },
+   "Venezuela": {
+      "Total": 37
+   },
+   "Yemen": {
+      "Total": 36
+   }
+};
+
+export const IMPORT_DATA = {
+   "Australia": {
+      "Total": 410
+   },
+   "Austria": {
+      "Total": 13
+   },
+   "Bulgaria": {
+      "Total": 9
+   },
+   "Canada": {
+      "Total": 1831
+   },
+   "Czech Republic": {
+      "Total": 28
+   },
+   "France": {
+      "Total": 612
+   },
+   "Germany (FRG)": {
+      "Total": 1467
+   },
+   "Israel": {
+      "Total": 763
+   },
+   "Italy": {
+      "Total": 355
+   },
+   "Japan": {
+      "Total": 40
+   },
+   "Jordan": {
+      "Total": 20
+   },
+   "Netherlands": {
+      "Total": 308
+   },
+   "New Zealand": {
+      "Total": 75
+   },
+   "Norway": {
+      "Total": 736
+   },
+   "Peru": {
+      "Total": 4
+   },
+   "Poland": {
+      "Total": 160
+   },
+   "Russia": {
+      "Total": 16
+   },
+   "Saudi Arabia": {
+      "Total": 18
+   },
+   "South Africa": {
+      "Total": 426
+   },
+   "Spain": {
+      "Total": 310
+   },
+   "Sweden": {
+      "Total": 90
+   },
+   "Switzerland": {
+      "Total": 1244
+   },
+   "Ukraine": {
+      "Total": 66
+   },
+   "United Kingdom": {
+      "Total": 2712
+   }
+};
--- src/index.js
+++ src/index.js
@@ -18,3 +18,14 @@ const map = new Datamap(
 window.addEventListener('resize', function() {
     map.resize();
 });
+
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        alert(xmlhttp.responseText);
+    }
+};
+
+xmlhttp.open("GET", "us_exports.csv", true);
+xmlhttp.send();
