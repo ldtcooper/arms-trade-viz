f3f5145de2be0033cc224043846d4bf2335538d5
Logan Cooper <ldtcoop@gmail.com>
2017-10-03 13:49:11 -0700

    Add mouseover handlers for arcs

--- src/index.js
+++ src/index.js
@@ -32,7 +32,16 @@ const map = new Datamap(
   }
 );
 
+// map.arc(exportArcsGenerator(EXPORT_DATA));
+d3.selectAll('.datamaps-subunit')
+  .on('mouseover', function(d, i) {
+    map.arc([{origin: 'USA', destination: d.id, strokeWidth: 1}]);
+  });
 
+d3.selectAll('.datamaps-subunit')
+  .on('mouseout', function(d, i) {
+    map.arc([]);
+  });
 
 window.addEventListener('resize', function() {
     map.resize();
